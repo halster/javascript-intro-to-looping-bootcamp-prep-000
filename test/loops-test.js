e64c3fc404d2df030b16d15e9b78f9f42e808d24
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-20 00:40:14 +0000

    automatically backed up by learn

--- test/loops-test.js
+++ test/loops-test.js
@@ -65,3 +65,17 @@ describe('loops', () => {
     })
   })
 })
+function forLoop(array){
+  for (let i=0, i<25,i++){
+    if (i===1){
+      array[1]="I am 1 strange loop."
+    }
+    else {
+      array[i]=`I am ${i} strange loops.`
+    }
+  }
+  return array
+}
+function whileLoop(number){
+
+}
