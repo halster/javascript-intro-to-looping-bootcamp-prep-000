e1100024f9eb9523088557f24c5a3aea3e73817b
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-20 00:25:38 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop(array ){
   for (let i=0, i<25,i++){
-  array[i]="I am ${i} strange loops."
+    if (i===1){
+      array[1]="I am 1 strange loop."
+    }
+    else {
+      array[i]="I am ${i} strange loops."
+    }
   }
   return array
 }
