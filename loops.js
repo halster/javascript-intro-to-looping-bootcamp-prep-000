49f63fb719e3762e0991a831ab7fdf68957f3652
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-20 01:11:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,13 @@ var array={}
 function forLoop(array){
   for (let i=0; i<25;i++){
     if (i===1){
-      array[1]=`I am 1 strange loop.`
-    }
-    else {
-      array[i]=`I am ${i} strange loops.`
-    }
+  array[1]=`I am 1 strange loop.`
+}
+else {
+  array[i]=`I am ${i} strange loops.`
+}
+
+    
   }
 return array
 }
@@ -17,4 +19,13 @@ while(number>0){
   console.log(--number)
 }
 return "done"
+}
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array.pop()
+  } while (array.length >0 && maybeTrue());
 }
